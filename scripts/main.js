deb63868d8ba7d4e4fafaa01c51de85e4d054d95
Mitchell-Liu <32404146+Mitchell-Liu@users.noreply.github.com>
2022-05-21 19:05:31 -0700

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,4 @@
-const myHeading = document.querySelector('h1');
-myHeading.textContent = 'Hello world!';
+
 document.querySelector('html').addEventListener('click', () => {
   alert('Ouch! Stop poking me!');
 });
@@ -7,15 +6,21 @@ let myButton = document.querySelector('button');
 let myHeading = document.querySelector('h1');
 function setUserName() {
   let myName = prompt('Please enter your name.');
-  localStorage.setItem('name', myName);
-  myHeading.textContent = 'Mozilla is cool, ' + myName;
+  if(!myName) {
+    setUserName();
+  } else {
+    localStorage.setItem('name', myName);
+    myHeading.innerHTML = 'Mozilla is cool, ' + myName;
+  }
 }
+
 if(!localStorage.getItem('name')) {
   setUserName();
 } else {
   let storedName = localStorage.getItem('name');
-  myHeading.textContent = 'Mozilla is cool, ' + storedName;
+  myHeading.innerHTML = 'Mozilla is cool, ' + storedName;
 }
+
 myButton.onclick = function() {
   setUserName();
 }
